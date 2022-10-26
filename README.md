c4165c56888de6ebd680d6194620560aeb7c9f62
cortega <cortega@acciontip.cl>
2022-10-26 10:45:16 -0500

    documentacion y package start

---
+++ README.md
@@ -0,0 +1,25 @@
+# test bsale
+
+uso del sistema back test de ventas
+
+para la utilización solo debe acceder al link
+https://bsaleback12.herokuapp.com/
+
+---------------------------------------------------------------
+Instalación y descarga del proyecto
+
+
+Clonar backend donde guste
+git clone https://github.com/crsbl/bsaleBack.git
+
+correr cmd en el directorio origen del backend
+teclear node index.js 
+
+
+
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const { createServer } = require("http");
 
+  
 const httpServer = createServer((req, res) => {
   const peticiones = require("./peticiones/index.js");
   //filtro GET o POST
   switch (req.method) {
     case "GET":
+      //Esta funcion acumula las funciones get
       peticiones.peticionesGet(req, res);
       break;
     case "POST":
+       //Esta funcion acumula las funciones post
       peticiones.peticionesPost(req, res);
       break;
     case "OPTIONS":
